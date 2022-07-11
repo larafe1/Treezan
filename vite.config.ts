7f9c9698bbc7b61fc069454ceda4ea68b00b6012
Felipe Lara <felipeahlara@gmail.com>
2022-07-11 18:23:32 -0300

    Create Vite App

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.join(__dirname, "src") }],
+  },
+  plugins: [react()],
+});
