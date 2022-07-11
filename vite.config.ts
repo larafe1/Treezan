59ddefb951ad860bb5074822d8737945307a33d0
Felipe Lara <felipeahlara@gmail.com>
2022-07-11 19:13:51 -0300

    Chore: Styled-components and vitest added

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
-    logOverride: { "this-is-undefined-in-esm": "silent" },
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
   },
   resolve: {
-    alias: [{ find: "@", replacement: path.join(__dirname, "src") }],
+    alias: [{ find: '@', replacement: path.join(__dirname, 'src') }]
   },
-  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html']
+    },
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '**/.{idea,git,cache,output,temp}/**'
+    ]
+  },
+  plugins: [react()]
 });
