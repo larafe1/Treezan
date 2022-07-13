62e99f3da6ec544ab5f0bba6c90d19f6935c40a2
Felipe Lara <felipeahlara@gmail.com>
2022-07-13 19:47:25 -0300

    Bug fix: Removing next to eslint plugins and adding an space between imports on AppProvider

--- src/providers/AppProvider/index.tsx
+++ src/providers/AppProvider/index.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle } from '@/styles/global';
 import { theme } from '@/styles/theme';
+
 import type { AppProviderProps } from './types';
 
 export const AppProvider = ({ children }: AppProviderProps) => {
